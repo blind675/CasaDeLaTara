a0aba99cc6ff963f7e4538b455146872a81558c6
Catalin-Andrei BORA <catalin.bora@gmail.com>
2015-12-16 17:31:35 +0200

    work on admin integration.

--- endpoint-backend/src/main/webapp/admin/js/controller.js
+++ endpoint-backend/src/main/webapp/admin/js/controller.js
@@ -7,11 +7,11 @@ var app = angular.module('casaDeLaTara', [
 app.run(['GAuth', 'GApi',
   function(GAuth, GApi) {
 
-    if ($window.location.hostname == 'localhost') {
-      BASE = '//localhost:8080/_ah/api';
-    } else {
+//    if ($window.location.hostname == 'localhost') {
+//      BASE = '//localhost:8080/_ah/api';
+//    } else {
       BASE = 'https://genuine-axle-114721.appspot.com';
-    }
+//    }
 
     GApi.load('casaDeLaTaraEndpoint', 'v1', BASE);
     GAuth.setScope('https://www.googleapis.com/auth/userinfo.email');
